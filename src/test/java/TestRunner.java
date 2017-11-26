f02c161762045a4f6dfd5aaba5fc5fbd7e194688
jimmylobo <jimmy.lobo@gmail.com>
2017-11-26 20:00:37 +0100

    Zmiana ścieżki do glue.

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -2,7 +2,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@CucumberOptions(features = {"src/test/resources/cukes"}, glue = {"src/main/StepDefs"})
+@CucumberOptions(features = {"src/test/resources/cukes"}, glue = {"StepDefs"})
 @RunWith(Cucumber.class)
 public class TestRunner {
 }
